65091ec2e4b9ec23e5d6933bd97d51c5284812ba
Dylan Mooers <dmooers@calpoly.edu>
2021-03-09 11:37:28 -0800

    Added in built-in functions and base environment

--- A9.ex
+++ A9.ex
@@ -1,6 +1,6 @@
 defmodule Interp do
     @type exprC :: NumC.t() | IdC.t() | StrC.t() | AppC.t() | LamC.t() | IfC.t() 
-    @type environment :: list(Binding.t())
+    @type environment :: Map
     @type value :: NumV.t() | PrimV.t() | CloV.t() | BoolV.t() | StrV.t()
 
     defmodule NumC do
@@ -106,4 +106,61 @@ defmodule Interp do
                     raise "WTUQ invalid function call"
         end
     end
+
+    @doc """
+        Basic wrapper for binomial mathematical expressions
+    """
+    @spec binom(Function) :: (list(Interp.value) -> Interp.value)
+    def binom(func) do
+        fn vals ->
+            case vals do
+                [%NumV{num: num1}, %NumV{num: num2}] ->
+                    func.(num1, num2)
+                _ ->
+                    raise "WTUQ: Wrong arg types provided"
+            end
+        end
+    end    
+    add = fn num1, num2 -> %NumV{num: num1 + num2} end
+    multiply = fn num1, num2 -> %NumV{num: num1 * num2} end
+    sub = fn num1, num2 -> %NumV{num: num1 - num2} end
+    lessThanEq = fn num1, num2 -> %BoolV{bool: num1 <= num2} end
+    div = fn num1, num2 -> 
+        case num2 do
+            0 -> raise "WTUQ: Divide by 0 err"
+            _ -> %NumV{num: num1 / num2}
+        end
+    end
+    eq = fn values ->
+        case values do 
+            [%NumV{num: n1}, %NumV{num: n2}] -> %BoolV{bool: n1 == n2}
+            [%StringV{str: str1}, %StringV{str2}] -> 
+                %BoolV{bool: String.equivalent?(str1, str2)}
+            [%BoolV{bool: b1}, %BoolV{bool: b2}] -> %BoolV{bool: b1 == b2}
+            _ -> raise "WTUQ: Incorrect use of equal?"
+        end
+    end
+    myError = fn values ->
+        case values do
+            [a] -> raise a
+            _ -> raise "Inccorect use of error"
+        end
+    end
+
+    newEnv = %{
+        true: %BoolV{bool: true},
+        false: %BoolV{bool: false},
+        +: Interp.binom(add),
+        *: Interp.binom(multiply),
+        /: Interp.binom(div),
+        -: Interp.binom(sub),
+        <=: Interp.binop(lessThanEq),
+        equal?: eq,
+        error: myError
+    }
+    
+
+
 end
+
+
